2b45ca4c42eea56e2df817a0082766b749941c11
dkaman79 <73869964+dkaman79@users.noreply.github.com>
2021-11-27 12:31:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor((Math.random()*6)+1);
+document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+
+var randomNumber2 = Math.floor((Math.random()*6)+1);
+document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+
+
+
+if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML =  "Draw!";
+}
+else if (randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player1 wins!";
+}
+else if(randomNumber2>randomNumber1) {
+    document.querySelector("h1").innerHTML =  "Player2 wins! 🚩";
+}
+
+
+console.log(randomNumber1);
